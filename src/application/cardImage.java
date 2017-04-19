022d7eea41ad3428156bc736bcf58508edae34f8
racer161 <ethan@spurlock.io>
2017-04-19 11:19:38 -0500

    Ok its really fixed this time :100:

--- src/application/cardImage.java
+++ src/application/cardImage.java
@@ -5,7 +5,7 @@ import javafx.scene.image.ImageView;
 
 public class cardImage 
 {
-	private Image[] cardImages = new Image[53];
+	private Image[] cardImages = new Image[54];
 
 	public cardImage()
 	{
